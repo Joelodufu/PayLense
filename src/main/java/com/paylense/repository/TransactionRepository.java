f0f96c07d6c1726fa1f86620c07447bdd0aff09a
Joel Odufu <joel.odufu@gmail.com>
2025-11-30 10:40:34 +0100

    Update TransactionRepository with method to find transactions by sender or receiver wallet number ordered by creation date descending

--- src/main/java/com/paylense/repository/TransactionRepository.java
+++ src/main/java/com/paylense/repository/TransactionRepository.java
@@ -1,11 +1,11 @@
 package com.paylense.repository;
 
-import com.paylense.domain.Transaction;
+import com.paylense.domain.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    List<Transaction> findBySourceWalletIdOrDestinationWalletIdOrderByTimestampDesc(String sourceWalletId, String destinationWalletId);
+    List<Transaction> findBySenderWalletWalletNumberOrReceiverWalletWalletNumberOrderByCreatedAtDesc(String senderWalletNumber, String receiverWalletNumber);
 }
