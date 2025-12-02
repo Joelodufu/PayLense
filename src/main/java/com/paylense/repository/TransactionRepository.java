b2458aa14903741658cdae563b7cbbaa28dc58c0
Joel Odufu <joel.odufu@gmail.com>
2025-12-02 01:00:31 +0100

    Merge TransactionRepository methods from both versions, include @Repository annotation and all specified methods

--- src/main/java/com/paylense/repository/TransactionRepository.java
+++ src/main/java/com/paylense/repository/TransactionRepository.java
@@ -1,11 +1,21 @@
 package com.paylense.repository;
 
-import com.paylense.domain.entity.Transaction;
+import com.paylense.model.Transaction;
+import com.paylense.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
     List<Transaction> findBySenderWalletWalletNumberOrReceiverWalletWalletNumberOrderByCreatedAtDesc(String senderWalletNumber, String receiverWalletNumber);
+
+    List<Transaction> findBySenderWalletOrReceiverWallet(Wallet senderWallet, Wallet receiverWallet);
+
+    List<Transaction> findBySenderWallet(Wallet senderWallet);
+
+    List<Transaction> findByReceiverWallet(Wallet receiverWallet);
+
 }
